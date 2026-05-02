06450c49bcb05de5fcc7a16575ff4f8e23fc931e
vonMitzscha <support@mitzscherling.digital>
2026-05-02 19:07:28 +0200

    basic structure

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#CB4D1B',
+          stroke:  '#802400',
+          box:     '#F9A887',
+        },
+      },
+      fontFamily: {
+        serif: ['"Hoefler Text"', 'Georgia', 'serif'],
+        sans:  ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
