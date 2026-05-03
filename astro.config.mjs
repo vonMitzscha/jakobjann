dfafc106f33337d62d8cb284b7069537ada4956c
vonMitzscha <support@mitzscherling.digital>
2026-05-03 20:21:34 +0200

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://vonmitzscha.github.io',
+  base: '/jakobjann',
   integrations: [tailwind()],
   output: 'static',
   outDir: './dist',
